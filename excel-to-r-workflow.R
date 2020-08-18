865fad48b78c11fa966ee91fae53b4cfcfff11ee
Matt Farrow <matt@formalizedcuriosity.net>
2020-08-18 10:26:46 -0600

    commit initial files

---
+++ excel-to-r-workflow.R
@@ -0,0 +1,15 @@
+
+# Setup -------------------------------------------------------------------
+
+# Install "tidyverse" packages
+install.packages("tidyverse")
+
+# Load tidyverse packages
+library(tidyverse)
+
+# Load data
+
+# Data Manipulation -------------------------------------------------------
+
+
+
